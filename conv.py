89a104038d561d73d0e20e4a994d1046dc7e4235
ExtremeSitting <sam@txlans.com>
2012-11-23 01:38:26 -0600

    Fixed PEP8 formatting

--- conv.py
+++ conv.py
@@ -7,54 +7,57 @@
 import argparse
 import string
 
+
 def main():
 
-    parser = argparse.ArgumentParser(description='Usage: %prog [value] [size]',
+    parser = argparse.ArgumentParser(
+        description='Usage: %prog [value] [size]',
         epiloge='Converts file size into equivilents.')
     parser.add_argument(
-      'integer', metavar='INT', type=float, help='value in b, kb, mb, gb or tb')
+        'integer', metavar='INT', type=float,
+        help='value in b, kb, mb, gb or tb')
     parser.add_argument(
         'size', metavar='SIZE', type=str, help='indicate the size',
         choices='b, k, m, g, t')
 
     args = parser.parse_args()
     num = args.integer + 0.0
-    
+
     vals = []
     sizes = ['Bytes:', 'Kilobytes:', 'Megabytes:', 'Gigabytes:', 'Terabytes:']
     if args.size == 'b':
         vals.append(num)
         vals.append(num / 1024)
-        vals.append(num / 1024/1024)
-        vals.append(num / 1024/1024/1024)
-        vals.append(num / 1024/1024/1024/1024)
+        vals.append(num / 1024 / 1024)
+        vals.append(num / 1024 / 1024 / 1024)
+        vals.append(num / 1024 / 1024 / 1024 / 1024)
     elif args.size == 'k':
         vals.append(num * 1024)
         vals.append(num)
         vals.append(num / 1024)
-        vals.append(num / 1024/1024)
-        vals.append(num / 1024/1024/1024)
+        vals.append(num / 1024 / 1024)
+        vals.append(num / 1024 / 1024 / 1024)
     elif args.size == 'm':
         vals.append(num * 1024)
-        vals.append(num * 1024*1024)
+        vals.append(num * 1024 * 1024)
         vals.append(num)
         vals.append(num / 1024)
-        vals.append(num / 1024/1024)
+        vals.append(num / 1024 / 1024)
     elif args.size == 'g':
-        vals.append(num * 1024*1024*1024)
-        vals.append(num * 1024*1024)
+        vals.append(num * 1024 * 1024 * 1024)
+        vals.append(num * 1024 * 1024)
         vals.append(num * 1024)
         vals.append(num)
         vals.append(num / 1024)
     elif args.size == 't':
-        vals.append(num * 1024*1024*1024*1024)
-        vals.append(num * 1024*1024*1024)
-        vals.append(num * 1024*1024)
+        vals.append(num * 1024 * 1024 * 1024 * 1024)
+        vals.append(num * 1024 * 1024 * 1024)
+        vals.append(num * 1024 * 1024)
         vals.append(num * 1024)
         vals.append(num)
 
     for s, n in zip(sizes, vals):
-        print string.rjust(s, 10), string.rjust(str(n),2)
+        print string.rjust(s, 10), string.rjust(str(n), 2)
 
 
 if __name__ == '__main__':
